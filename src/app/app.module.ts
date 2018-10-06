3293612e3f29a4617b2131fa59edf6ce26a12cc2
Karthik Prasad <adkp.mlr@gmail.com>
2018-10-07 00:14:09 +0530

    Added home page and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,25 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing';
+
+import { MatToolbarModule, MatSidenavModule, MatIconModule, MatRippleModule, MatMenuModule, MatButtonModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    AuthModule.forRoot(),
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatToolbarModule, MatSidenavModule, MatIconModule, MatRippleModule, MatMenuModule, MatButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'my-login',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  faSpinner = faSpinner;
+  isInProgress = false;
+  returnUrl: string;
+  error = '';
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required
+    ]),
+    password: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  constructor(private auth: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    // reset login status
+    this.auth.logout();
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '';
   }
 
+  onSubmit() {
+    if (this.loginForm.controls.username.invalid && this.loginForm.controls.username.invalid) {
+      return;
+    }
+    this.loginForm.setErrors({ loginFailed: false });
+    this.isInProgress = true;
+    const username = this.loginForm.controls.username.value;
+    const password = this.loginForm.controls.password.value;
+    this.auth.login(username, password).subscribe(r => {
+      localStorage.setItem('currentUser', JSON.stringify({
+        username: username,
+        token: r.headers.get('x-auth-token')
+      }));
+    }, e => {
+      console.log(e);
+      this.isInProgress = false;
+      this.loginForm.setErrors({ loginFailed: true });
+    }, () => {
+      this.isInProgress = false;
+      this.router.navigate([this.returnUrl]);
+    });
+  }
 }
